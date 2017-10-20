f9bcfa2f889562c76d24a6bfc86acd70388545a1
Alexander Schwartzberg <aeksco@gmail.com>
2017-10-20 17:02:52 -0400

    Moved server code into web_api/ subdirectory

--- web_api/server/routes.js
+++ web_api/server/routes.js
@@ -1,11 +1,9 @@
 const router = require('express').Router()
-// const authMiddleware = require('./middleware/auth')
-// const auth = require('./auth/router')
 
 // // // //
 
 // Bootstrap API routers
-router.use('/auth', require('./api/user'))
+router.use('/auth', require('./api/auth'))
 router.use('/users', require('./api/user'))
 
 // // // //
