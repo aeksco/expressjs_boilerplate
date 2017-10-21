923ea7d7e5c2a87d6aac3bfa5ecc0defdf5fe928
Alexander Schwartzberg <aeksco@gmail.com>
2017-10-21 15:17:58 -0400

    ElasticSearch pagination

--- web_api/server/api/car/car.controller.js
+++ web_api/server/api/car/car.controller.js
@@ -5,14 +5,23 @@ const Car = require('./car.model');
 
 // // // //
 
+
+
+// // // //
+
 // GET /cars
 // TODO - pagination (middleware?)
 module.exports.list = (req, res, next) => {
 
-    esodm.connect('eb_test_index').then(function(){
-      // be sure to call connect before bootstrapping your app.
-      Car.find().then(function(documents){
-        console.log(documents);
+    let queryOptions = {
+        page:       Number(req.query.page) || 0,
+        per_page:   Number(req.query.per_page) || 10,
+    };
+
+    let query = {};
+
+    esodm.connect('cars').then(function(){
+      Car.find(query, queryOptions).then((documents) => {
         res.send(documents).end();
       });
     });
@@ -25,12 +34,14 @@ module.exports.list = (req, res, next) => {
 module.exports.create = (req, res, next) => {
 
     // TODO - abstract connect into bin/www.js
-    return esodm.connect('eb_test_index').then(function(){
-      // be sure to call connect before bootstrapping your app.
-      Car.save().then(function(document){
-        console.log(document);
-        res.send(document).end();
+    return esodm.connect('cars').then(() => {
+
+      let newCar = new Car({ type: req.body.type, color: req.body.color });
+
+      newCar.save().then((response) => {
+        res.send(response).end();
       });
+
     });
 };
 
@@ -40,7 +51,7 @@ module.exports.create = (req, res, next) => {
 module.exports.show = (req, res, next) => {
 
     // TODO - abstract into /bin/www.js
-    return esodm.connect('eb_test_index').then( () => {
+    return esodm.connect('cars').then( () => {
         Car.findById(req.params.id).then( (response) => {
             return res.status(200).send(response).end();
         });
@@ -53,7 +64,7 @@ module.exports.show = (req, res, next) => {
 module.exports.update = (req, res, next) => {
 
     // TODO - abstract into /bin/www.js
-    return esodm.connect('eb_test_index').then( () => {
+    return esodm.connect('cars').then( () => {
         Car.findById(req.params.id).then( (doc) => {
             doc.update(req.body).then( (response) => {
                 return res.status(200).send(response).end();
@@ -67,7 +78,7 @@ module.exports.update = (req, res, next) => {
 // DELETE /cars/:id
 module.exports.delete = (req, res, next) => {
     // TODO - abstract into /bin/www.js
-    return esodm.connect('eb_test_index').then( () => {
+    return esodm.connect('cars').then( () => {
         Car.remove(req.params.id).then( (response) => {
             return res.status(200).send(response).end();
         });
